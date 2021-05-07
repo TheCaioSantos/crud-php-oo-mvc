4c31faef507a5d46100809067b3f853629264c09
Caio Santos <contactcaiosantos@gmail.com>
2021-05-06 21:11:40 -0300

    Update router.php

--- controller/router.php
+++ controller/router.php
@@ -13,7 +13,7 @@ if (isset($_GET['pagina']) && $pagina = trim($_GET['pagina'])) {
 		}
 	} else {
 		//Se o arquivo não existir, o usuário é redirecionado para pagina '404.php'
-		require_once "view/erro/404";
+		require_once "view/erro/404.php";
 	}
 } else {
 	//Se nenhuma pagina for passada, o usuário é redirecionado para pagina 'listar.php'
